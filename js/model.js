7ca9555ab91c1d75b64fe8d396f5742fd44bc8b3
udayvakiti <98404979+udayvakiti@users.noreply.github.com>
2023-08-25 18:18:39 +0530

    Update model.js

--- js/model.js
+++ js/model.js
@@ -1,1 +1,30 @@
-
+async function classifyImage() {
+    const imageElement = document.getElementById('imageInput');
+    const resultElement = document.getElementById('result');
+    
+    // Load the MobileNet model.
+    const model = await mobilenet.load();
+    
+    // Get the selected image file.
+    const imageFile = imageElement.files[0];
+    
+    if (imageFile) {
+        const image = document.createElement('img');
+        const reader = new FileReader();
+        
+        reader.onload = async (e) => {
+            image.src = e.target.result;
+            await image.onload;
+            
+            // Make a prediction.
+            const predictions = await model.classify(image);
+            
+            // Display the prediction results.
+            resultElement.innerHTML = `Prediction: ${predictions[0].className} (Probability: ${predictions[0].probability.toFixed(2)})`;
+        };
+        
+        reader.readAsDataURL(imageFile);
+    } else {
+        resultElement.innerHTML = 'Please select an image.';
+    }
+}
